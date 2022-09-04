0c52f81b40944cb24c9a2add38256745f05e8f6b
Gaëtan Maisse <gaetanmaisse@gmail.com>
2022-09-04 16:07:11 +0200

    Implement endpoints

--- src/book.controller.ts
+++ src/book.controller.ts
@@ -1,7 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { BookService } from './book.service';
+import { Book } from './Book';
 
-@Controller()
+@Controller('books')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @Get()
+  getBooks(@Query('author') author: string): Book[] {
+    if (author) {
+      return this.bookService.getBooksOf(author);
+    }
+    return this.bookService.getAllBooks();
+  }
+
+  @Post()
+  createBook(@Body() bookToCreate: Book): Book {
+    this.bookService.addBook(bookToCreate);
+    return this.bookService.getBook(bookToCreate.title);
+  }
+
+  @Get(':title')
+  getBookByTitle(@Param('title') title: string): Book {
+    return this.bookService.getBook(title);
+  }
+
+  @Delete(':title')
+  deleteBook(@Param('title') title: string): void {
+    return this.bookService.removeBook(title);
+  }
 }
--- src/book.service.ts
+++ src/book.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { Book } from './Book';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  private storedBooks: Book[] = [];
+
+  addBook(book: Book): void {
+    if (
+      !this.storedBooks.some((storedBook) => book.title === storedBook.title)
+    ) {
+      this.storedBooks.push(book);
+    }
+  }
+
+  getBook(title: string): Book {
+    const book = this.storedBooks.find((book) => book.title === title);
+    if (!book) {
+      throw new Error(`No book with title ${title}`);
+    }
+    return book;
+  }
+
+  getBooksOf(author: string): Book[] {
+    return this.storedBooks.filter((book) => book.author === author);
+  }
+
+  getAllBooks(): Book[] {
+    return this.storedBooks.sort((book1, book2) =>
+      book1.title.toLowerCase().localeCompare(book2.title.toLowerCase()),
+    );
+  }
+
+  getTotalNumberOfBooks(): number {
+    return this.storedBooks.length;
+  }
+
+  getBooksPublishedAfter(date: string | Date): Book[] {
+    const dateAsDate = new Date(date);
+
+    return this.storedBooks.filter(
+      (book) => new Date(book.date).getTime() >= dateAsDate.getTime(),
+    );
+  }
+
+  removeBook(title: string) {
+    this.storedBooks = this.storedBooks.filter((book) => book.title !== title);
+  }
+}
